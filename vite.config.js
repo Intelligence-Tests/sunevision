14b1031e2224e482033e4b1615380b6989c839d2
Intelligence-Tests <intelligencetests@gmail.com>
2024-11-24 13:57:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Explicitly define the output directory
-    rollupOptions: {
-      input: '/index.html', // Specify entry point for the build
-    },
+    outDir: 'dist',  // Explicitly define the output directory
   },
-});
-
+})
