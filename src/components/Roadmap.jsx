1a88110b476802dad16fe666cc85a38780c26c47
Intelligence-Tests <intelligencetests@gmail.com>
2024-12-01 19:54:39 +0530

    FinalUpdate

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -8,8 +8,7 @@ import { Gradient } from "./design/Roadmap";
 const Roadmap = ({ route }) => {
   const handleClick = () => {
     window.scrollTo(0, 0);
-  };
-
+  }
   return (
     <Section className="overflow-hidden" id="blog">
       <div className="container md:pb-10">
@@ -21,9 +20,8 @@ const Roadmap = ({ route }) => {
             return (
               <div
                 key={item.id}
-                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
-                  item.colorful ? "bg-conic-gradient" : "bg-n-6"
-                }`}
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                  }`}
               >
                 <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
                   <Gradient />
